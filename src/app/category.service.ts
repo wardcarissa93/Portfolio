453d7ea0d8ba564966891d022bac03d719d9d7d5
Carissa Ward <cward52@my.bcit.ca>
2024-03-10 10:27:48 -0700

    created services

---
+++ src/app/category.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Category } from './models/category';
+import { CATEGORIES } from './data/categories';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+  constructor() { }
+  getCategories(): Category[] {
+    return CATEGORIES;
+  }
+}
---
+++ src/app/tag.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Tag } from './models/tag';
+import { TAGS } from './data/tags';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TagService {
+  constructor() { }
+  getTags(): Tag[] {
+    return TAGS;
+  }
+}
