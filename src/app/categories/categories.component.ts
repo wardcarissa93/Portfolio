f0f8ff40ab75eefc3832092e4ac63150ec419e71
Carissa Ward <cward52@my.bcit.ca>
2024-03-10 10:59:17 -0700

    services are now used in the three components

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { Category } from '../models/category';
+import { CategoryService } from '../category.service';
 
 @Component({
   selector: 'app-categories',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.scss'
 })
 export class CategoriesComponent {
+  constructor(private categoryService: CategoryService) {}
 
+  categories: Category[] = [];
+  getCategories(): void {
+    this.categories = this.categoryService.getCategories();
+  }
+  ngOnInit(): void {
+    this.getCategories();
+  }
+
+  categoryFilter: Category | undefined;
+  
+  setCategoryFilter(category: Category) {
+    this.categoryFilter = category;
+  }
 }
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { Category } from '../models/category';
+import { Tag } from '../models/tag';
+import { Project } from '../models/project';
+
+import { ProjectService } from '../project.service';
+
+import { ProjectFilterPipe } from '../project-filter.pipe';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ProjectFilterPipe],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+  constructor(private projectService: ProjectService) {}
 
+  projects: Project[] = [];
+  getProjects(): void {
+    this.projects = this.projectService.getProjects();
+  }
+  ngOnInit(): void {
+    this.getProjects();
+  }
+
+  categoryFilter: Category | undefined;
+  tagFilter: Tag | undefined;
+
+  setCategoryFilter(category: Category) {
+    this.categoryFilter = category;
+  }
+
+  setTagFilter(tag: Tag) {
+    this.tagFilter = tag;
+  }
+
+  clearFilters() {
+    this.categoryFilter = undefined;
+    this.tagFilter = undefined;
+  }
 }
