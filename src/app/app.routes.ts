c4160eff1f97cf4fdf14ffa8b1e91dfbffa8cd09
Carissa Ward <cward52@my.bcit.ca>
2024-03-10 16:33:17 -0700

    finished steps for defining routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ProjectComponent } from './components/project/project.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'projects', component: ProjectsComponent, pathMatch: 'full' },
+];
