c208bd63f72b089808286a4e9792aa5ace4a80db
Carissa Ward <cward52@my.bcit.ca>
2024-03-13 15:40:22 -0700

    partial app module created

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app.routes'
+import { AppComponent } from './app.component';
+
+@NgModule({
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { ProjectComponent } from './components/project/project.component';
 
-export const routes: Routes = [
+const routes: Routes = [
     { path: 'projects', component: ProjectsComponent, pathMatch: 'full' },
     { path: 'projects/:id', component: ProjectComponent },
 ];
+
+export const AppRoutingModule = RouterModule.forRoot(routes);
