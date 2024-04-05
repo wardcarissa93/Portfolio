85046c6f57c516c55ea0ef6a8275be9725c69668
Carissa Ward <cward52@my.bcit.ca>
2024-04-04 17:23:42 -0700

    linked about page in app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { ProjectComponent } from './components/project/project.component';
 import { HomeComponent } from './components/home/home.component'
+import { AboutComponent } from './components/about/about.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent, pathMatch: 'full' },
+    { path: 'about', component: AboutComponent },
     { path: 'projects', component: ProjectsComponent },
     { path: 'projects/:id', component: ProjectComponent },
 ];
