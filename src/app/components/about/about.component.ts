2f970cad6815c269802c85fcf652def7e3d28d85
Carissa Ward <cward52@my.bcit.ca>
2024-04-05 18:47:39 -0700

    quite happy with the about page right now, waiting for feedback before merging and moving on to next step

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 
+import { HomeFish3Component } from '../home-fish-3/home-fish-3.component';
+import { Fish2Component } from '../fish-2/fish-2.component';
+
 @Component({
   selector: 'app-about',
   standalone: true,
   imports: [
+    HomeFish3Component,
+    Fish2Component,
   ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
