1a54a68c62d493e091753a4e3c79a9039f260eb8
Carissa Ward <cward52@my.bcit.ca>
2024-04-06 20:48:16 -0700

    not found page completed

--- src/app/components/not-found/not-found.component.ts
+++ src/app/components/not-found/not-found.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 
+import { HomeFish3Component } from '../home-fish-3/home-fish-3.component';
+import { Fish2Component } from '../fish-2/fish-2.component';
+
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [],
+  imports: [
+    HomeFish3Component,
+    Fish2Component,
+  ],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss'
 })
